cd6953a8d282648c37980d261e3ac2beb25ed0bb
Inwoo Hwang <dlsdndls@naver.com>
2026-03-19 18:07:24 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 81.47 ms, Memory: 197 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -0,0 +1,49 @@
+// 알고리즘
+// 스택
+
+import java.util.*;
+
+class Solution {
+    
+    public int[] solution(int[] numbers) {
+        
+        // 정답 배열
+        int[] answer = new int[numbers.length];
+        
+        // 스택 생성
+        Stack<int[]> stack = new Stack<>();
+        
+        for(int i=0; i<numbers.length; i++){
+            
+            // 1. 현재 인덱스 값
+            int cur = numbers[i];
+            // 2. 스택이 비어있는 경우
+            if(!stack.isEmpty()) {
+                // 현재 수가 더 큰 경우
+                if(cur > stack.peek()[1]){
+                    // 비어있지 않는 경우 반복
+                    while(!stack.isEmpty()){
+                        // 스택에 탑이 더 큰경우는 종료
+                        if(stack.peek()[1] >= cur ){
+                            break;
+                        }        
+                        // 스택 꺼내기 pop
+                        answer[stack.pop()[0]] = cur;
+                    }   
+                }
+                
+                // 스택 넣어주고 종료
+                
+            }
+            stack.push(new int[]{i,cur});
+        }
+        // 남은 스택 처리 후 종료
+        while(!stack.isEmpty()){
+            int[] cur = stack.pop();
+            answer[cur[0]] = -1;
+        }
+        
+        return answer;
+    }
+    
+}
