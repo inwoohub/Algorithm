10e421d8c558a7824f201a9de9cf396fee21235c
황인우 <dlsdndls@naver.com>
2026-04-02 09:55:35 +0900

    [level 3] Title: 여행경로, Time: 0.60 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -7,11 +7,12 @@
     4. 경로 2개 이상이면 알파벳 순서가 앞서기
     
 전략 : (dfs + backtracking)
-    1. ArrayList 만들기 (단방향 연결 가능하게)
-    2. tickets 꺼내보면서 단방향 연결 해주기 (여러개 가능함으로 index 표기해주기)
-    3. ArrayList 사전순으로 정렬해두기
-    4. dfs로 방문하기, 정답 인덱스에 하나씩 담기
-    5. 틀리다면 방문 해제 하면서 정답 인덱스 다시 !
+    1. ArrayList 만들기                      (단방향 연결 용도) ex) 'ICN' -> 'JFK', 'ICN' -> 'SFO' ...
+    2. tickets 꺼내보면서 단방향 연결 해주기       (데이터 매핑) 
+    3. ArrayList에서 단어 사전순으로 정렬해두기     (정렬)
+    4. dfs로 방문하기, 정답 인덱스에 하나씩 담기     (dfs 탐색) 
+    5. 완성하지 못한 경우 dfs 빠져나오면서 방문 해제  (backtracking)
+    6. 끝가지 방문했을 때 True 반환하며 dfs 벗어나기 (종료 조건)
 */
 
 import java.util.*;
@@ -19,20 +20,15 @@ import java.util.*;
 class Solution {
     
     static int ICN; // 시작 여행지는 인천이므로 인천 위치 저장용도
-    static HashMap<String, Integer> map ; // 스트링 -> Int 로 변환용도
-    static ArrayList<Trip>[] list;
-    static boolean[] visited;
-    static int indexMax;
-    static int ticketsSize;
+    static HashMap<String, Integer> map = new HashMap<>(); // 스트링 -> Int 로 변환용도
+    static ArrayList<Trip>[] list; // 여행 경로를 담은 ArrayList
+    static boolean[] visited; // 방문 배열
+    static int indexMax; // 여행 경로 크기
     
     public String[] solution(String[][] tickets) {
         
-        ticketsSize = tickets.length;
-        
-        map = new HashMap<>();
-        
         indexMax = tickets.length + 1;
-        ICN = -1; // 인천 배열 index 초기 위치
+        ICN = -1; // 인천 배열 index 초기값 세팅 ('-1')
         visited = new boolean[tickets.length+1]; // 방문 처리용 배열 생성
         
         // 1. list 배열 생성 후 초기화 (맵 연결 용도)
@@ -41,68 +37,52 @@ class Solution {
             list[i] = new ArrayList<>();
         }
         
-        // 해당 티켓에 맞는 int (배열에 넣을 매핑 용도)
-        int count = 0;
-        int idx = 0;
+        int count = 0; // ex) "ICN" -> '0', 'SFO' -> '1' 처럼 HashMap 써서 변환 용도
+        int idx = 0;   // ex) "ArrayList[]" 배열에 인덱스
                 
         // 2. 여행지 리스트로
         for(int i=0; i<tickets.length; i++){
             String[] ticket = tickets[i];
             
-            // 해당 ticket이 없다면 count, 있다면 해당 매핑된 count 가져오기
+            // '출발지' 에 맞는 리스트의 idx 가져오기
             int getT = map.getOrDefault(ticket[0], count);
             
-            // if(getT != count && ticket[0].equals("ICN")){
-            //     ICN = getT;
-            // }
-            
-            // 처음 여행지 발견
-            if(getT == count){
-                map.put(ticket[0], count);
-                list[count].add(new Trip(ticket[1], idx)); // 단방향 연결
+            if(getT == count){ // 만약, 처음 '출발지' 발견
+                map.put(ticket[0], count); // String -> Integer 로 HashMap 사용해서 변환
+                list[count].add(new Trip(ticket[1], idx)); // 출발지 -> 도착지로 단방향 연결
                 idx++;
                 if(ticket[0].equals("ICN")){
+                    ICN = count; // 인천 발견 시 Index 저장
+                }
+                count++;
+            } else { // '출발지' 첫 발견이 아니라면,
+                list[getT].add(new Trip(ticket[1], idx)); // 출발지 -> 도착지로 단방향 연결
+                idx++;
+            }
+            
+            // '도착지' 에 맞는 리스트의 idx 가져오기
+            getT = map.getOrDefault(ticket[1], count);
+            
+            if(getT == count){ // 만약, 처음 '도착지' 발견
+                map.put(ticket[1], count);
+                if(ticket[1].equals("ICN")){
                     ICN = count; // 인천 Index 저장
                 }
                 count++;
-            } else {
-                // 기존 여행지 경우
-                list[getT].add(new Trip(ticket[1], idx)); // 단방향 연결
-                idx++;
-            }
-            
-            // 도착지도 map에 등록 (없을 때만)
-            if(!map.containsKey(ticket[1])){
-                map.put(ticket[1], count);
-                if(ticket[1].equals("ICN")){
-                    ICN = count;
-                }
-                count++;
-            }
-            
+            }   
         }
         
         // 3. ArrayList[] 여행지명 기준 사전순으로 정렬
         for(int i=0; i<count; i++){
-            Collections.sort(list[i], (a,b) -> a.region.compareTo(b.region) );
+            Collections.sort(list[i]);
         }
         
-        // 정답용 String 리스트
+        // 정답용 String 리스트 생성
         ArrayList<String> result = new ArrayList<>();
         result.add("ICN"); // 인천 시작
         
         // 4. 시작지가 인천인 경우로 시작
-        for(int i=0; i<list[ICN].size(); i++){
-            Trip cur = list[ICN].get(i);
-            result.add(cur.region);
-            visited[cur.index] = true; // 방문 처리
-            int searchIdx = map.get(cur.region); // 다음 여행지 배열의 인덱스
-            boolean check = search(result, searchIdx, 2);
-            if(check) break; // 탐색 완료시 종료
-            
-            result.remove(1); // 백트래킹 리스트에서 제거
-            visited[cur.index] = false; // 백트래킹 방문해제
-        }
+        search(result, ICN, 1);
         
         String[] answer = new String[result.size()];
         for(int i=0; i<result.size(); i++){
@@ -114,38 +94,42 @@ class Solution {
     // dfs 탐색 시작
     static boolean search(ArrayList<String> result, int searchIdx, int idx){
         
-        if(indexMax == idx ){
-            return true;
-        }
+        if(indexMax == idx ) return true; // 종료! (모두 찾은 경우)
+        
         for(int i=0; i<list[searchIdx].size(); i++){
             Trip cur = list[searchIdx].get(i);
-            if(visited[cur.index]) continue; // 방문했던 여행지라면
+            if(visited[cur.index]) continue; // 방문했던 여행지라면 넘어가기
             
             // 처음 방문한다면,
             result.add(cur.region);
             visited[cur.index] = true;
             int nextIdx = map.get(cur.region);
-            // System.out.println(cur.region + " 방문");
-            boolean check = search(result, nextIdx, idx+1);
-            if(check){ // 탐색 성공 시
-                return true;
-            }
-            result.remove(result.size() - 1); // 백트래킹 리스트에서 제거
+            boolean check = search(result, nextIdx, idx+1); // 다음 dfs 탐색
+            
+            if(check) return true;  // 종료! (탐색 성공 시)
+            
+            result.remove(idx); // 백트래킹 리스트에서 제거
             visited[cur.index] = false; // 백트래킹 방문해제   
         }
         return false;   
     }
     
-    
     // trip (ArrayList 에 담을 타입)
     // region : 지역
     // index : 방문 처리용 index
-    static class Trip{
+    static class Trip implements Comparable <Trip> {
         String region;
         int index;
+        
         Trip(String region, int index){
             this.region = region;
             this.index = index;
         }
+        
+        @Override
+        public int compareTo(Trip other) {
+            return (this.region).compareTo(other.region);  // compare → compareTo
+        }
+        
     }
 }
