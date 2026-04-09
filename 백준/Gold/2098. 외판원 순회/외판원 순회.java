ae814632e9f2a74c4d06b32a9a1d1b3d65193f35
황인우 <dlsdndls@naver.com>
2026-04-09 20:48:20 +0900

    [Gold I] Title: 외판원 순회, Time: 640 ms, Memory: 22324 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,76 +1,97 @@
-// ---------------------------
-// 알고리즘
-// bitMask 를 활용한 dfs
-// & 와 | 비트 연산자를 통해서 방문 여부 및 비트마스크 계산
-// 싸이크링 존재하기 때문에 어느 곳에서 시작하더라도 상관없어서 0으로 시작.
-// 
-// 1. 비트마스킹을 통해 방문 했다면 통과
-// 2. map 거리가 0 즉, 방문 못할시 통과
-// 3. 현재 mask == maxMask 즉, 모든 정점 도달 했을 경우, return 마지막노드->0 으로 
-// 4. 이미 방문했다라면, 바로 값 반환
-// ---------------------------
-// 주요 변수
-// maxMask, map, dp
-// ---------------------------
+/**
+알고리즘 :
+    TSP (Traveling Salesman problem)
+    -> 영업사원이 여러 도시를 한 번씩만 방문하고 출발지로 돌아오는 최단 경로를 찾는 조합 최적화 문제
 
-import java.io.*;
+문제 요약 :
+    출발지를 제외하고 모든경로를 딱 1번만 순회하며 여행했을 때 최소 비용
+
+전략 :
+    1. dist[][] 거리를 담아주는 배열 만들기
+    2. dp[][] 현재 노드에 있을 때 이전 경로를 지나온 경로중 최소 비용 담는 배열 만들기
+    3. 모든 노드를 순회하며 출발지로 설정
+    4. dp[][] 배열의 크기 즉, 비트마스크 만큼 for문 돌리기
+    5. 현재 위치해 있는 노드가 비트마스크로 비교하였을 때 이전 경로에 포함되어있는지 확인하기
+        - 포함 되어있지 않다면 4번으로
+    6. 다음 방문할 노드를 아직 방문 안했는지 확인하기
+        - 포함 되어있다면 이미 방문했으므로 넘어가기
+    7. 다음 노드 방문 할 때 더 적은 비용으로 접근 가능 하다면 업데이트
+    8. 4 ~7 이 끝난 후 최소 비용 갱신 (작은 경우만)
+*/
+
 import java.util.*;
-
+import java.io.*;
 
 public class Main{
 
-    static int N;
-    static int maxMask;
-    static int[][] map;
-    static int[][] dp;
-
     public static void main(String[] args) throws IOException{
+
+        int answer = Integer.MAX_VALUE; // 결과 초기값 세팅
+        
+        // 데이터 입력 및 매핑
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        N = Integer.parseInt(st.nextToken());
-        maxMask = (1<<N)-1;
-        map = new int[N][N];
-        dp = new int[N][maxMask+1];
-        
-        
+
+        int N = Integer.parseInt(st.nextToken()); // 노드 개수
+
+        // 1, 2 배열 생성
+        int[][] dist = new int[N][N];  // 각 노드끼리의 길이 배열 생성
+        int[][] dp = new int[N][1<<N]; // dp 배열 생성 [N]: 현재 위치, [1<<N] : 비트마스크 -> 0001 은 1번 노드 방문, 0011 은 1번,2번 노드 방문 비트로 마스킹하기
+
+        // dist 배열에 거리 넣어주기
         for(int i=0; i<N; i++){
-            Arrays.fill(dp[i], -1);
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+                dist[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int ans = dfsAndDP(0, 1);
-        System.out.print(ans);
-        
-    }
-
-    static int dfsAndDP(int cur, int mask){
-        // 기존에 이미 방문했던거라면 바로 반환
-        if(dp[cur][mask] != -1 ) return dp[cur][mask];
-        
-        if(mask == maxMask){
-            // 방문 불가능
-            if(map[cur][0] == 0){
-                return 1000000000;
+        // 3. 모든 노드가 출발지가 가능하기 때문에 for문 사용
+        for(int i=0; i<N; i++){
+            
+            // dp 배열 초기 세팅 (최소를 찾기 위해 최대로 설정)
+            for(int j=0; j<N; j++){
+                Arrays.fill(dp[j], Integer.MAX_VALUE);
             }
-            return map[cur][0];
+            // dp 첫번째 세팅
+            dp[i][1<<i] = 0; // i에 있고, i만 방문했으니까 0 넣어주기 (이동 안한 상태)
+
+            // 4. 비트 마스크 만큼 for 문 돌리기 (모든 경로 중 최소 값 찾기 위해서)
+            for(int mask=0; mask<(1<<N); mask++){
+
+                // 5. 현재 위치 (모든 노드가 현재 위치가 가능)
+                for(int cur=0; cur<N; cur++){
+                    // 현재 위치가 이전 방문 경로에 포함되어있는지 확인, 아니라면 패스
+                    if( (mask&(1<<cur)) == 0 ) continue; // 포함되어있지 않음
+                    if( dp[cur][mask] == Integer.MAX_VALUE ) continue; // 갱신 못하고 오버플로우 방지
+
+                    // 6. 다음 방문노드 탐색
+                    for(int next=0; next<N; next++){
+                        // 다음 위치가 방문 안한 위치인지 확인하기
+                        if( (mask & (1<<next)) != 0 ) continue; // 마스크에 포함되어있음
+
+                        // 길이 없어도 통과
+                        if( dist[cur][next] == 0 ) continue;
+                        
+                        
+                        // 7. 더 적은 비용으로 업데이트 하기 ( 이전 최소 비용 값 vs 현재->다음 비용 값 )
+                        dp[next][ (mask|(1<<next)) ] = Math.min( dp[next][(mask|(1<<next))], dp[cur][mask]+dist[cur][next] );
+
+                        // System.out.println("["+ next + "]["+(mask|(1<<next))+"] : "+dp[next][ (mask|(1<<next)) ] + "  , mask : "+mask);
+                    }
+                }
+            }
+            
+            // 4 ~ 7 종료 후 마지막 노드 -> 출발지로 돌아가는 길 작은걸로 업데이트
+            for(int last=0; last<N; last++){
+                if(last == i) continue; // 마지막 == 출발지 라면 성립 불가능
+                if(dist[last][i] == 0) continue; // 길이 없는 경우도 못돌아감
+                if(dp[last][(1<<N)-1] == Integer.MAX_VALUE) continue; // 오버 플로우 방지
+                answer = Math.min( answer, dp[last][(1<<N)-1] + dist[last][i] ); // 이전 최소 비용 vs 새로운 최소 비용 비교 (전부 방문 + 출발지)
+            }
         }
-
-        int best = 1000000000;
-        for(int next=0; next<N; next++){
-            // 이미 방문했다면 통과
-            if( (mask & ( 1<< next)) != 0 ) continue;
-            // 길 없음
-            if(map[cur][next] == 0 ) continue;
-
-            int cand = map[cur][next] + dfsAndDP( next, mask | (1<<next) );
-
-            best = Math.min(cand, best);
-        }
-        dp[cur][mask] = best;
-        return best;
+        // 결과 출력
+        System.out.println(answer);
     }
 }
+
