4862fe1e399e75fc5f0766762b203681818291c2
황인우 <dlsdndls@naver.com>
2026-08-03 16:20:32 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 4.25 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -3,102 +3,66 @@ import java.util.*;
 class Solution {
     
     public int solution(String str1, String str2) {
-        // 1. str1 두 글자씩 끊어서 ArrayList에 넣기
         ArrayList<String> listA = new ArrayList<>();
         cutting(listA, str1);
-        
-        // 2. str2 두 글자씩 끊어서 ArrayList에 넣기
         ArrayList<String> listB = new ArrayList<>();
         cutting(listB, str2);
-        
-        // 3. 자카드 유사도
-        int jaccard = jaccardSearch(listA, listB);
-        
-        return jaccard;
+        return jaccardSearch(listA, listB);
     }
     
     static int jaccardSearch(ArrayList<String> listA, ArrayList<String> listB) {
-        // 1. 해당 문자열 몇개있는지 개수세기
         HashMap<String, Integer> mapA = new HashMap<>();
         HashMap<String, Integer> mapB = new HashMap<>();
-        
         for(String cur : listA) {
             int curCnt = mapA.getOrDefault(cur, 0);
-            curCnt++;
-            mapA.put(cur, curCnt);
+            mapA.put(cur, ++curCnt);
         }
         for(String cur : listB) {
             int curCnt = mapB.getOrDefault(cur, 0);
-            curCnt++;
-            mapB.put(cur, curCnt);
+            mapB.put(cur, ++curCnt);
         }
-        
-        // 교집합 만들기 
-        int A = 0; // 교집합 개수
+        int A = 0;
         HashMap<String, Boolean> map = new HashMap<>();
         for(String cur : listA) {
-            if(!listB.contains(cur)) continue; // 없음
-            boolean check = map.getOrDefault(cur, false);
-            if(check) continue; // 이미 처리
-            int cntA = mapA.get(cur);
-            int cntB = mapB.get(cur);
-            A += Math.min(cntA, cntB);
-            map.put(cur, true);  // 사용 처리
+            if(!listB.contains(cur)) continue;
+            if(map.getOrDefault(cur, false)) continue;
+            A += Math.min(mapA.get(cur), mapB.get(cur));
+            map.put(cur, true);
         }
-        
-        int B = 0; // 합집합 개수
+        int B = 0;
         map = new HashMap<>();
-        // 합집합 만들기 (listA 처리)
         for(String cur : listA) {
-            boolean check = map.getOrDefault(cur, false);
-            if(check) continue; // 이미 처리
+            if(map.getOrDefault(cur, false)) continue;
             if(!listB.contains(cur)) {
                 B += mapA.get(cur);
-            } else { // 겹치는 경우
-                int cntA = mapA.get(cur);
-                int cntB = mapB.get(cur);
-                B += Math.max(cntA, cntB);
+            } else {
+                B += Math.max(mapA.get(cur), mapB.get(cur));
             }
-            map.put(cur, true);  // 사용 처리
+            map.put(cur, true);
         }
-        // 합집합 만들기 (listB 처리)
         for(String cur : listB) {
-            boolean check = map.getOrDefault(cur, false);
-            if(check) continue; // 이미 처리
+            if(map.getOrDefault(cur, false)) continue;
             B += mapB.get(cur);
-            map.put(cur, true);  // 사용 처리
+            map.put(cur, true);
         }
-        if(A==0 && B==0) {
-            return 65536;
-        } else if (A==0) {
-            return 0;
-        }
+        if(A==0 && B==0) return 65536;
         return (65536 * A) / B;
     }
-    
-    
     
     static void cutting(ArrayList<String> list, String str) {
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<str.length()-1; i++){
-            char firstChar = str.charAt(i);
-            char secondChar = str.charAt(i+1);
-            // 1. 대문자/소문자 구분 없게 모두 소문자로 변환
-            firstChar = genderChar(firstChar);
-            secondChar = genderChar(secondChar);
-            // 2. 특수문자, 공백 확인
+            char firstChar = genderChar(str.charAt(i));
+            char secondChar = genderChar(str.charAt(i+1));
             if((int) firstChar<97 || 122 < (int) firstChar) continue;
             if ((int) secondChar<97 || 122 < (int) secondChar) continue;
-            // 3. 문자 더하기
             sb = new StringBuilder();
             sb.append(firstChar);
             sb.append(secondChar);
-            // 4. 리스트에 추가
             list.add(sb.toString());
         }
     }
     
-    // 대/소문자 -> 소문자로 변환 젠더
     static Character genderChar(char A) {
         int curInt = (int) A;
         if(curInt<97 || curInt>122) {
@@ -106,5 +70,4 @@ class Solution {
         }
         return (char) curInt;
     }
-    
 }
